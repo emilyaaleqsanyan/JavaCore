35610cf9d3f977f3b322f6beb2457e0433937236
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2023-08-25 18:18:05 +0400

    Array

---
+++ src/classwork/chapter3/AutoArray.java
@@ -0,0 +1,8 @@
+package classwork.chapter3;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int mnth_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("В апрелье " + mnth_days[3] + " дней.");
+    }
+}
