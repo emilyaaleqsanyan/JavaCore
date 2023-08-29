c12d41c00d99b2c9d4545a2ad9acfb77a75a7228
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2023-08-29 15:36:32 +0400

    I made changes 2

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -25,43 +25,24 @@ public class FigurePainter {
         System.out.println();
         System.out.println();
 
-        for (i = 1; i < 6; i++) {
-            for (int j = 1; j < 6; j++) {
-                if (j > 5 - i) {
-                    System.out.print(a);
-                }
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
 
+        System.out.println();
+        System.out.println();
         for (i = 0; i < 5; i++) {
-            for (int j = 1; j < 5; j++) {
-                if (j > i) {
-                    System.out.print(" ");
-                }
-                System.out.print(a);
-            }
-            System.out.println();
-        }
-
-        System.out.println();
-        System.out.println();
-        for(i = 0; i < 5; i++ ){
-            for(int j = 5; j > i; j--){
+            for (int j = 5; j > i; j--) {
                 System.out.print(" ");
             }
-            for(int j = 0; j <= i; j++){
+            for (int j = 0; j <= i; j++) {
                 System.out.print(a + " ");
             }
             System.out.println();
         }
-        for(i = 0; i <4; i++){
-            for(int j = 0; j < i; j++){
+        for (i = 0; i < 4; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print(" ");
             }
-            for(int j = 4; j > i; j --){
-                System.out.print(" " +  a);
+            for (int j = 4; j > i; j--) {
+                System.out.print(" " + a);
             }
             System.out.println();
         }
@@ -69,11 +50,11 @@ public class FigurePainter {
 
         System.out.println();
         System.out.println();
-        for(i = 0; i < 5; i++){
-            for(int j = 5; j > i; j--){
+        for (i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
                 System.out.print(" ");
             }
-            for(int j = 0; j <= i; j++){
+            for (int j = 0; j <= i; j++) {
                 System.out.print(a);
             }
             System.out.println();
@@ -81,11 +62,11 @@ public class FigurePainter {
 
         System.out.println();
         System.out.println();
-        for(i = 0; i < 5; i++){
-            for(int j = 0; j < i; j++){
+        for (i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for(int j = 5; j > i; j--){
+            for (int j = 5; j > i; j--) {
                 System.out.print(a);
             }
             System.out.println();
