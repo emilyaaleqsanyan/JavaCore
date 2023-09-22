f855b0613ca0670c4a950d8465b78d531cff9d5f
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2023-09-22 23:15:20 +0400

    add deleteByIndex, set, add, exists,getIndexByValue

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -9,7 +9,7 @@ public class DynamicArray {
     //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
     //և ավելացնենք
     public void add(int value) {
-        if(size == array.length) {
+        if (size == array.length) {
             extend();
         }
         array[size++] = value;
@@ -20,7 +20,7 @@ public class DynamicArray {
     //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
     private void extend() {
         int[] array1 = new int[array.length + 10];
-        for(int i = 0; i < array.length; i ++){
+        for (int i = 0; i < array.length; i++) {
             array1[i] = array[i];
         }
         array = array1;
@@ -29,19 +29,68 @@ public class DynamicArray {
     //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
     // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
     public int getByIndex(int index) {
-        if(index >= 0 && index < array.length - 1){
-        return array[index];
+        if (index >= 0 && index < array.length - 1) {
+            return array[index];
         }
         return -1;
     }
 
     //տպել մասիվի ավելացված էլեմենտները
     public void print() {
-        for(int i = 0; i < size; i ++){
+        for (int i = 0; i < size; i++) {
             System.out.print(array[i]);
         }
-
+        System.out.println();
     }
 
+    public void deleteByIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Токой элемент не существует");
+            return;
+        }
+        for (int i = index; i < size-1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+    }
+
+
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println("Токой элемент не существует");
+        } else array[index] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println("Токой элемент не существует");
+        }
+        if(size == array.length) {
+            extend();
+        }
+        size++;
+        for (int i = size - 1; i >=index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+    }
+
+    public boolean exists(int value) {
+        for (int i : array) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
--- src/homework/dynamicarray/TestDynamicArray.java
+++ src/homework/dynamicarray/TestDynamicArray.java
@@ -1,24 +1,34 @@
 package homework.dynamicarray;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class TestDynamicArray {
     public static void main(String[] args) {
         DynamicArray da = new DynamicArray();
         da.add(6);
-        da.add(6);
-        da.add(6);
-        da.add(6);
-        da.add(6);
-        da.add(6);
         da.add(4);
+        da.add(7);
+        da.add(2);
+        da.add(4);
+        da.add(3);
+        da.add(1);
         da.add(9);
-        da.add(7);
-        da.add(7);
-        da.add(7);
-        da.add(7);
-        System.out.println(da.getByIndex(7));
-        System.out.println(da.getByIndex(555));
+        da.add(4);
+        da.add(1);
+
         da.print();
-        System.out.println(da.getByIndex(5));
+
+        //System.out.println(da.getByIndex(555));
+//        da.print();
+        //System.out.println(da.getByIndex(5));
+//        da.deleteByIndex(7);
+       // System.out.println();
+        //da.print();
+        //da.set(1,7);
+//        da.add(5,9);
+//        System.out.println (da.exists(8));
+        System.out.println(da.getIndexByValue(4));
+        da.print();
 
     }
 
