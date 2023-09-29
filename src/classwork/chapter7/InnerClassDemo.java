51562c3249bb611a78d25bfce18b715666eb3e47
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2023-09-29 21:21:20 +0400

    chapter 7 VarArgs

---
+++ src/classwork/chapter7/InnerClassDemo.java
@@ -0,0 +1,10 @@
+package classwork.chapter7;
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.test();
+        OuterFor ou = new OuterFor();
+        ou.test();
+    }
+}
---
+++ src/classwork/chapter7/VarArgs2.java
@@ -0,0 +1,19 @@
+package classwork.chapter7;
+
+public class VarArgs2 {
+    static void vaTest(String msg, int... v) {
+        System.out.print(msg + v.length + " Садержимое: ");
+        for (int x : v) {
+            System.out.print(x + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        vaTest("Один параметор переменной длины: ", 10);
+        vaTest("Три параметра переменной длины: ", 1, 2, 3);
+        vaTest("Без параметров переменной длины: ");
+    }
+
+}
+
